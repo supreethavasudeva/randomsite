281523a9951b99b3fe2a2464b02837f3b7ee6ad3
Supreetha K V <144483014+supreethavasudeva@users.noreply.github.com>
2024-08-13 18:47:33 +0530

    Update init.js

--- Pendo/init.js
+++ Pendo/init.js
@@ -1,6 +1,4 @@
-
-(function(apiKey){
-    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+(function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
     v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
@@ -27,5 +25,5 @@
              monthly_value: '1000'
             }
         });
-})('19d279d9-f5fe-4614-7b1c-20469d39913a');
+})('19d279d9-f5fe-4614-7b1c-20469d39913a');</script>
 
